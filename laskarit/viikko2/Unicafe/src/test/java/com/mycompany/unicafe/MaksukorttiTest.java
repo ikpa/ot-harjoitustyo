029d905e927275a7571977c06ed70e6b1b582d5e
Patrik Ikäheimonen <patrik.ikaheimonen@helsinki.fi>
2020-03-19 14:36:40 +0200

    netbeans tehtäviä

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -10,11 +10,44 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    
+    @Test
+    public void kortillaOikeaMaara() {
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    
+    @Test
+    public void laataaminenKasvattaaMaaraa() {
+        kortti.lataaRahaa(1000);
+        assertEquals("saldo: 20.0", kortti.toString());
+    }
+    
+    @Test
+    public void voiOttaaRahaaKunTarpeeksi() {
+        kortti.otaRahaa(500);
+        assertEquals("saldo: 5.0", kortti.toString());
+    }
+    
+    @Test
+    public void eiVoiOttaaRahaaKunEiTarpeeksi() {
+        kortti.otaRahaa(1100);
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    
+    @Test
+    public void palauttaaTrueKunRahatRiittaa() {
+        assertTrue(kortti.otaRahaa(500));
+    }
+    
+    @Test
+    public void palauttaaFalseKunRahaEiRiita() {
+        assertFalse(kortti.otaRahaa(1100));
     }
 }
